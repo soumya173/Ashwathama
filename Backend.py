2db2642472b1b5685b9b4f2fed470abd456c441e
Soumyajit Gorai <soumya173@gmail.com>
2019-10-20 12:49:23 +0530

    Added few basic questions

--- Backend.py
+++ Backend.py
@@ -1,136 +1,175 @@
 import re
+import random
 import sqlite3
 from collections import Counter
 from string import punctuation
 from math import sqrt
-from nltk.corpus import words as nltk_words
+# from nltk.corpus import words as nltk_words
+# from nltk.corpus import wordnet
 
 #####################################################
 # Installation Notes for nltk.corpus.words
 #
 # Step 1 : First install nltk
-# 	- pip install nltk
+#   - pip install nltk
 # Step 2 : Enter into python shell
-# 	- python
+#   - python
 # Step 3 : inside python shell download english words from nktl library (this step requires internet connection)
-# 	- nltk.download('words')
+#   - nltk.download('words')
 #
 
 class Backend(object):
-	def __init__(self):
-	    """initialize the connection to the database"""
-	    self.connection = sqlite3.connect('chatbot.sqlite')
-	    self.cursor = self.connection.cursor()
+    def __init__(self):
+        """initialize the connection to the database"""
+        self.connection = sqlite3.connect('chatbot.sqlite')
+        self.cursor = self.connection.cursor()
 
-	    self.bot_msg = 'Hello'
+        self.bot_msg = 'Hello'
 
-	    self.init_db()
+        self.init_db()
 
-	def init_db(self):
-	    """create the tables needed by the program"""
-	    create_table_request_list = [
-	        'CREATE TABLE IF NOT EXISTS words(word TEXT UNIQUE)',
-	        'CREATE TABLE IF NOT EXISTS sentences(sentence TEXT UNIQUE, used INT NOT NULL DEFAULT 0)',
-	        'CREATE TABLE IF NOT EXISTS associations (word_id INT NOT NULL, sentence_id INT NOT NULL, weight REAL NOT NULL)',
-	    ]
-	    for create_table_request in create_table_request_list:
-	        try:
-	            self.cursor.execute(create_table_request)
-	        except:
-	            pass
+    def init_db(self):
+        """create the tables needed by the program"""
+        create_table_request_list = [
+            'CREATE TABLE IF NOT EXISTS words(word TEXT UNIQUE)',
+            'CREATE TABLE IF NOT EXISTS sentences(sentence TEXT UNIQUE, used INT NOT NULL DEFAULT 0)',
+            'CREATE TABLE IF NOT EXISTS associations (word_id INT NOT NULL, sentence_id INT NOT NULL, weight REAL NOT NULL)',
+        ]
+        for create_table_request in create_table_request_list:
+            try:
+                self.cursor.execute(create_table_request)
+            except:
+                pass
 
-	def get_id(self, entityName, text):
-	    """Retrieve an entity's unique ID from the database, given its associated text.
-	    If the row is not already present, it is inserted.
-	    The entity can either be a sentence or a word."""
-	    tableName = entityName + 's'
-	    columnName = entityName
-	    self.cursor.execute('SELECT rowid FROM ' + tableName + ' WHERE ' + columnName + ' = ?', (text,))
-	    row = self.cursor.fetchone()
-	    if row:
-	    	return row[0]
-	    else:
-	        self.cursor.execute('INSERT INTO ' + tableName + ' (' + columnName + ') VALUES (?)', (text,))
-	        return self.cursor.lastrowid
+    def get_id(self, entityName, text):
+        """Retrieve an entity's unique ID from the database, given its associated text.
+        If the row is not already present, it is inserted.
+        The entity can either be a sentence or a word."""
+        tableName = entityName + 's'
+        columnName = entityName
+        self.cursor.execute('SELECT rowid FROM ' + tableName + ' WHERE ' + columnName + ' = ?', (text,))
+        row = self.cursor.fetchone()
+        if row:
+            return row[0]
+        else:
+            self.cursor.execute('INSERT INTO ' + tableName + ' (' + columnName + ') VALUES (?)', (text,))
+            return self.cursor.lastrowid
 
-	def get_words(self, text):
-	    """Retrieve the words present in a given string of text.
-	    The return value is a list of tuples where the first member is a lowercase word,
-	    and the second member the number of time it is present in the text."""
-	    wordsRegexpString = '(?:\w+|[' + re.escape(punctuation) + ']+)'
-	    wordsRegexp = re.compile(wordsRegexpString)
-	    wordsList = wordsRegexp.findall(text.lower())
-	    return Counter(wordsList).items()
+    def get_words(self, text):
+        """Retrieve the words present in a given string of text.
+        The return value is a list of tuples where the first member is a lowercase word,
+        and the second member the number of time it is present in the text."""
+        wordsRegexpString = '(?:\w+|[' + re.escape(punctuation) + ']+)'
+        wordsRegexp = re.compile(wordsRegexpString)
+        wordsList = wordsRegexp.findall(text.lower())
+        return Counter(wordsList).items()
 
-	def clean_tables(self):
-	    """This will clear all the table data required for chat.
-	    WARNING : Do not call this method unless you want to reset everything. This is dangerous."""
-	    truncate_table_request_list = [
-	        'TRUNCATE TABLE words',
-	        'TRUNCATE TABLE sentences',
-	        'TRUNCATE TABLE associations',
-	    ]
-	    for truncate_table_request in truncate_table_request_list:
-	        try:
-	            self.cursor.execute(truncate_table_request)
-	        except:
-	            pass
+    def clean_tables(self):
+        """This will clear all the table data required for chat.
+        WARNING : Do not call this method unless you want to reset everything. This is dangerous."""
+        truncate_table_request_list = [
+            'TRUNCATE TABLE words',
+            'TRUNCATE TABLE sentences',
+            'TRUNCATE TABLE associations',
+        ]
+        for truncate_table_request in truncate_table_request_list:
+            try:
+                self.cursor.execute(truncate_table_request)
+            except:
+                pass
 
-	def basic_validation(self, words):
-		# Get the list of valid words from nltk dictionary
-		set_of_words = set(nltk_words.words())
+    def basic_validation(self, words):
+        # Get the list of valid words from nltk dictionary
+        # set_of_words = set(nltk_words.words())
 
-		for word in words:
-			# Check if the input word is a valid english word
-			if word not in set_of_words:
-				return False
+        # for word in words:
+            # Check if the input word is a valid english word
+            # if word not in set_of_words:
+            #   return False
 
-		return True
 
-	def process_known_questions(self, question):
-		pass
+            # if not wordnet.synsets(word):
+            #     return False
 
-	def process(self, input_str):
-	    # store the association between the bot's message words and the user's response
-	    words = self.get_words(self.bot_msg)
+        return True
 
-	    # If input word is not valid, stop processing
-	    if not self.basic_validation(words) :
-	    	self.bot_msg = "Please enter valid english words."
-	    	return self.bot_msg
+    def filter_punctuation(self, inp_str):
+        input_str.replace(".", "")
+        input_str.replace(",", "")
+        input_str.replace("?", "")
 
-	    # Check for basic questions, if known answer immediately
-	    if self.process_known_questions(input_str) :
-	    	return self.bot_msg
+        return inp_str
 
-	    words_length = sum([n * len(word) for word, n in words])
-	    sentence_id = self.get_id('sentence', input_str)
-	    for word, n in words:
-	        word_id = self.get_id('word', word)
-	        weight = sqrt(n / float(words_length))
-	        self.cursor.execute('INSERT INTO associations VALUES (?, ?, ?)', (word_id, sentence_id, weight))
-	    self.connection.commit()
+    def process_known_questions(self, question):
+        greetings_text = ["hi", "hello", "greetings"]
+        greetings_resp = ["Hey", "Hello there", "Hello", "Hey, what's up?", "Hi", "Hi, how can I help you?"]
 
-	    # retrieve the most likely answer from the database
-	    self.cursor.execute('CREATE TEMPORARY TABLE results(sentence_id INT, sentence TEXT, weight REAL)')
-	    words = self.get_words(input_str)
-	    words_length = sum([n * len(word) for word, n in words])
-	    for word, n in words:
-	        weight = sqrt(n / float(words_length))
-	        self.cursor.execute('INSERT INTO results SELECT associations.sentence_id, sentences.sentence, ?*associations.weight/(4+sentences.used) FROM words INNER JOIN associations ON associations.word_id=words.rowid INNER JOIN sentences ON sentences.rowid=associations.sentence_id WHERE words.word=?', (weight, word,))
+        question = question.lower()
+        question = self.filter_punctuation(question)
 
-	    # if matches were found, give the best one
-	    self.cursor.execute('SELECT sentence_id, sentence, SUM(weight) AS sum_weight FROM results GROUP BY sentence_id ORDER BY sum_weight DESC LIMIT 1')
-	    row = self.cursor.fetchone()
-	    self.cursor.execute('DROP TABLE results')
+        if question in greetings_text:
+            self.bot_msg = random.choice(greetings_resp)
+            return
 
-	    # otherwise, just randomly pick one of the least used sentences
-	    if row is None:
-	        self.cursor.execute('SELECT rowid, sentence FROM sentences WHERE used = (SELECT MIN(used) FROM sentences) ORDER BY RANDOM() LIMIT 1')
-	        row = self.cursor.fetchone()
+        if re.match("^how are.*", question):
+            self.bot_msg = random.choice(["Everything's fine", "Absolutely fine"])
+            return
 
-	    # tell the database the sentence has been used once more, and prepare the sentence
-	    self.bot_msg = row[1]
-	    self.cursor.execute('UPDATE sentences SET used=used+1 WHERE rowid=?', (row[0],))
+        if re.match("what is .* name$", question):
+            self.bot_msg = "My name is Ashwathama"
+            return
 
-	    return self.bot_msg
+        if re.match("who (created|made) you", question):
+            self.bot = "Some developer :)"
+            return
+
+        if re.match("college fees", question) or re.match("college department", question) or re.match("college faculty", question) or re.match("college library", question):
+            self.bot = "Please find all these info in the official college website"
+            return
+
+    def process(self, input_str):
+        inp_words = input_str.split(" ")
+
+        # If input word is not valid, stop processing
+        if not self.basic_validation(inp_words) :
+            self.bot_msg = "Please enter valid english words."
+            return self.bot_msg
+
+        # Check for basic questions, if known answer immediately
+        if self.process_known_questions(input_str) :
+            return self.bot_msg
+
+        # store the association between the bot's message words and the user's response
+        words = self.get_words(self.bot_msg)
+
+        words_length = sum([n * len(word) for word, n in words])
+        sentence_id = self.get_id('sentence', input_str)
+        for word, n in words:
+            word_id = self.get_id('word', word)
+            weight = sqrt(n / float(words_length))
+            self.cursor.execute('INSERT INTO associations VALUES (?, ?, ?)', (word_id, sentence_id, weight))
+        self.connection.commit()
+
+        # retrieve the most likely answer from the database
+        self.cursor.execute('CREATE TEMPORARY TABLE results(sentence_id INT, sentence TEXT, weight REAL)')
+        words = self.get_words(input_str)
+        words_length = sum([n * len(word) for word, n in words])
+        for word, n in words:
+            weight = sqrt(n / float(words_length))
+            self.cursor.execute('INSERT INTO results SELECT associations.sentence_id, sentences.sentence, ?*associations.weight/(4+sentences.used) FROM words INNER JOIN associations ON associations.word_id=words.rowid INNER JOIN sentences ON sentences.rowid=associations.sentence_id WHERE words.word=?', (weight, word,))
+
+        # if matches were found, give the best one
+        self.cursor.execute('SELECT sentence_id, sentence, SUM(weight) AS sum_weight FROM results GROUP BY sentence_id ORDER BY sum_weight DESC LIMIT 1')
+        row = self.cursor.fetchone()
+        self.cursor.execute('DROP TABLE results')
+
+        # otherwise, just randomly pick one of the least used sentences
+        if row is None:
+            self.cursor.execute('SELECT rowid, sentence FROM sentences WHERE used = (SELECT MIN(used) FROM sentences) ORDER BY RANDOM() LIMIT 1')
+            row = self.cursor.fetchone()
+
+        # tell the database the sentence has been used once more, and prepare the sentence
+        self.bot_msg = row[1]
+        self.cursor.execute('UPDATE sentences SET used=used+1 WHERE rowid=?', (row[0],))
+
+        return self.bot_msg
